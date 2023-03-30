04f91c0dc453e8b0c918e563d2483e205d80bf5e
Marcel Jurec <marcel.jurec@seznam.cz>
2023-03-31 01:07:21 +0200

    adding USB MIDI support

--- StompBox/midi.h
+++ StompBox/midi.h
@@ -9,7 +9,7 @@
 class Midi
 {
 public:
-  enum MidiConnection {
+  enum MidiConnectionMode {
     Midi_Serial_9600    = 0,
     Midi_Serial_31250   = 1,
     Midi_USB            = 2,
@@ -86,20 +86,52 @@ public:
     Change_Next
   };
 
+  enum USB_CableId {
+    CId_Misc                  = 0x00, // Miscellaneous function codes. Reserved for future extensions
+    CId_Reserved              = 0x01, // Cable events. Reserved for future expansion.
+    CId_2BStream              = 0x02, // Two-byte System Common messages like MTC, SongSelect, etc.
+    CId_3BStream              = 0x03, // Three-byte System Common messages like SPP, etc
+    CId_SysExStartOrContinue  = 0x04, // SysEx starts or continues
+    CId_1BSysCommon           = 0x05, // Single-byte System Common Message
+    CId_SysExEndFollowing1B   = 0x05, // SysEx ends with following single byte
+    CId_SysExEndFollowing2B   = 0x06, // SysEx ends with following two bytes
+    CId_SysExEndFollowing3B   = 0x07, // SysEx ends with following three bytes.
+    CId_NoteOff               = 0x08, // Note-off
+    CId_NoteOn                = 0x09, // Note-on
+    CId_PolyKeyPress          = 0x0a, // Poly-KeyPress
+    CId_ControlChange         = 0x0b, // Control Change
+    CId_ProgramChange         = 0x0c, // Program Change
+    CId_ChannelPressure       = 0x0d, // Channel Pressure
+    CId_PitchBendChange       = 0x0e, // PitchBend Change
+    CId_1B                    = 0x0f  // Single Byte
+  };
+
   static const uint8_t c_VelocityMin = 0x00;
   static const uint8_t c_VelocityMax = 0x7f;
   static const uint8_t c_KeyMin = 0x00;
   static const uint8_t c_KeyMax = 0x7f;
 
-  Midi(HardwareSerial* pSerial = &Serial,
-    unsigned long baudRate = 31250,
+  Midi(MidiConnectionMode connectionMode = MidiConnectionMode::Midi_Default, 
     const bool skipSameCommands = true,
-    unsigned long skipSameMidiCommandsPeriod = 500) :
-    m_pSerial(pSerial),
-    m_skipSameCommands(skipSameCommands),
-    m_skipSameMidiCommandsPeriod(skipSameMidiCommandsPeriod)
-   {
-    m_pSerial->begin(baudRate);
+    unsigned long skipSameMidiCommandsPeriod = 500,
+    HardwareSerial* pSerial = &Serial) :
+  m_pSerial(pSerial),
+  m_skipSameCommands(skipSameCommands),
+  m_skipSameMidiCommandsPeriod(skipSameMidiCommandsPeriod)
+  {
+    switch(connectionMode) {
+      case MidiConnectionMode::Midi_Serial_9600:
+        m_pSerial->begin(9600); break;
+      case MidiConnectionMode::Midi_Serial_31250:
+        m_pSerial->begin(31250); break;
+    }
+  }
+
+  Midi(MidiConnectionMode connectionMode = MidiConnectionMode::Midi_Default) {
+    switch(connectionMode) {
+      case case MidiConnectionMode::Midi_USB:
+        m_usb = new MidiUSB();
+    }
   }
 
   ~Midi() {
@@ -115,6 +147,7 @@ public:
 private:
   //Circularuint8_tBuffer* m_pBuffer;
   HardwareSerial* m_pSerial = nullptr;
+  MidiUSB* m_usb = nullptr;
   bool m_skipSameCommands;
   unsigned long m_skipSameMidiCommandsPeriod;
 };
